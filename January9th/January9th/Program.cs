110d7b466c52c7ed26e972321db108d7fae9f996
Eric Charnesky <eric.charnesky@careevolution.com>
2020-01-09 19:12:39 -0500

    Hello World started

---
+++ January9th/January9th/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace January9th
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
