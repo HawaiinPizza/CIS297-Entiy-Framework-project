8cff9f13f27e00c1bf518751cb0ed2411babc2e2
Eric Charnesky <eric.charnesky@careevolution.com>
2020-01-09 20:57:15 -0500

    Triangle class

---
+++ January9th/January9th/Triangle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace January9th
+{
+    public class Triangle
+    {
+        private double _baseLength;
+        private double _height;
+
+        public void setBaseLength(double baseLength)
+        {
+            _baseLength = baseLength;
+        }
+
+        public void setHeight(double height)
+        {
+            _height = height;
+        }
+
+        public double getBaseLength()
+        {
+            return _baseLength;
+        }
+
+        public double getHeight()
+        {
+            return _height;
+        }
+
+        public double getArea()
+        {
+            return .5 * _baseLength * _height;
+        }
+    }
+}
